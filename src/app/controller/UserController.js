d7757226499a44ace8dde7bb3732c0d03631067f
Victor <victor@mark-I>
2020-01-20 15:26:55 -0300

    Remove Logs from UserController

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -4,12 +4,10 @@ class UserController {
   async findById(req, res) {
     try {
       const user = await User.findById(req.params.user_id);
-      console.log('user', user);
       if (!user) throw Error('UserNotFound');
       if (user.id !== req.tokenID) throw Error('TokenDoNotMatch');
       return res.status(200).send(user);
     } catch (error) {
-      console.log(error.message);
       switch (error.message) {
         case 'TokenDoNotMatch':
           return res.status(403).send({ message: 'Não autorizado' });
